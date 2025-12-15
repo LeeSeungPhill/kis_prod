036850c3e45176067aa2fd1887f63e9c1999d50c
LeeSeungPhill <125461810+LeeSeungPhill@users.noreply.github.com>
2025-12-15 22:10:26 +0900

    자선관리정보 신규생성 처리

--- stockFundMng/views.py
+++ stockFundMng/views.py
@@ -101,7 +101,7 @@ def list(request):
             n_cash_rate = 50  # 패턴 움직임
             n_asset_num = str(n_cash_rate) + n_asset_date
 
-            s1 = stock_fund_mng.objects.create(asset_num=int(n_asset_num), acct_no=acct_no, cash_rate=n_cash_rate)
+            s1 = stock_fund_mng.objects.create(asset_num=int(n_asset_num), acct_no=acct_no, cash_rate=n_cash_rate, sell_plan_amt=0, buy_plan_amt=0)
             s1.save()
 
     if stock_fund_mng.objects.filter(acct_no=acct_no).count() > 0:
@@ -180,14 +180,17 @@ def list(request):
                 #         asset_icdc_amt=u_asst_icdc_amt,  # 자산증감액
                 #         last_chg_date=datetime.now()
                 #     )
-
+                
+                u_cash_rate_amt = round(u_tot_evlu_amt * stock_fund_mng_info.cash_rate * 0.01, 0)  # 총평가금액 기준 현금 비중 금액
+                
                 stock_fund_mng.objects.filter(acct_no=acct_no, asset_num=stock_fund_mng_info.asset_num).update(
-                    tot_evlu_amt=u_tot_evlu_amt,  # 총평가금액
-                    dnca_tot_amt=u_dnca_tot_amt,  # 예수금 총금액
-                    prvs_rcdl_excc_amt=u_prvs_rcdl_excc_amt,  # 가수도 정산금액
-                    nass_amt=u_nass_amt,  # 순자산금액(세금비용 제외)
-                    scts_evlu_amt=u_scts_evlu_amt,  # 유저평가금액
-                    asset_icdc_amt=u_asst_icdc_amt,  # 자산증감액
+                    tot_evlu_amt=u_tot_evlu_amt,                # 총평가금액
+                    dnca_tot_amt=u_dnca_tot_amt,                # 예수금 총금액
+                    prvs_rcdl_excc_amt=u_prvs_rcdl_excc_amt,    # 가수도 정산금액
+                    nass_amt=u_nass_amt,                        # 순자산금액(세금비용 제외)
+                    scts_evlu_amt=u_scts_evlu_amt,              # 유저평가금액
+                    asset_icdc_amt=u_asst_icdc_amt,             # 자산증감액
+                    cash_rate_amt = u_cash_rate_amt,            # 총평가금액 기준 현금 비중 금액
                     last_chg_date=datetime.now()
                 )
 
